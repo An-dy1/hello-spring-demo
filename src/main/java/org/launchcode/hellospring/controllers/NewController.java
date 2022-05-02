8275d8572c3bfcc416b8aee4445c18103b98b0ed
Andie Johnson <ajohnson2@naic.org>
2022-05-02 14:08:46 -0500

    polishing hands on

--- src/main/java/org/launchcode/hellospring/controllers/NewController.java
+++ src/main/java/org/launchcode/hellospring/controllers/NewController.java
@@ -1,32 +1,45 @@
 package org.launchcode.hellospring.controllers;
 
+import org.apache.coyote.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class NewController {
 
-  @RequestMapping("about-us")
+  @RequestMapping("about-us-2")
   @ResponseBody
   public String getAboutUsPage() {
     return "<h1>About Us</h1>";
   }
 
-  @GetMapping("contact-us")
+  @GetMapping("contact-us-2")
   @ResponseBody
   public String getContactUsForm() {
-    return "<form action='contact-us' method='post'>" +
-              "<label for='email' />" +
+    // action: tells the form the URL where it will be processed
+    return "<form action='/contact-us-2' method='post'>" +
+              "<label for='email'>Email</label>" +
               "<input type='text' name='email'>" +
+              "<br>" +
+
+              "<label for='birth month'>Birth month:</label>" +
+              "<select name='birthMonth'>" +
+                "<option value='march'>March</option>" +
+                "<option value='april'>April</option>" +
+                "<option value='may'>May</option>" +
+              "</select>" +
+              "<br>" +
+
               "<input type='submit'>" +
             "</form>";
   }
 
-  @PostMapping("contact-us")
+  @PostMapping("contact-us-2")
   @ResponseBody
-  public String getCompletedForm(@RequestParam String email) {
+  public String getCompletedForm(@RequestParam String email, @RequestParam String birthMonth) {
     return "<p>Thank you for submitting your form!</p>" +
         "<p>We will contact you at:</p>" +
-        email;
+        email +
+        "<br><p>And tell you happy birthday in </p>" + birthMonth;
   }
 }
